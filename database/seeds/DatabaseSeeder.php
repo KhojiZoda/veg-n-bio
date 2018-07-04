65f1c521b5a80565647e23c715fc1130fda2eb90
KhojiZoda <kabdullazoda@gmail.com>
2018-07-04 16:47:40 +0200

    Fixed db connection added seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+          'first_name' => 'Kojak',
+          'last_name' => 'Zoda',
+          'birthday' => '1996-04-28',
+          'role' => 'admin',
+          'status' => 'active',
+          'email' => 'kabdullazoda@gmail.com',
+          'password' => bcrypt('Oklm1234'),
+          'address' => '10 rue Edouard Nortier',
+          'gender' => 'Homme'
+        ]);
     }
 }
