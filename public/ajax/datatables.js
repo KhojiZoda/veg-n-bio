37669375ecf6b33601ec6689471557aca944b175
KhojiZoda <kabdullazoda@gmail.com>
2018-07-04 09:28:07 +0200

    Fixed ajax errors

--- public/ajax/datatables.js
+++ public/ajax/datatables.js
@@ -1,7 +1,7 @@
 // Menu Edit
 $(document).ready(function(){
   var menuId = $('#menu_id');
-  if (menuId != undefined) {
+  if (menuId[0]) {
     $("table#menuMeals").dataTable({
       "language": {
         "url": "https://cdn.datatables.net/plug-ins/1.10.16/i18n/French.json"
@@ -51,7 +51,7 @@ $(document).ready(function(){
 $(document).ready(function(){
   var menuId = $('#menu_show_id');
 
-  if (menuId != undefined) {
+  if (menuId[0]) {
     $("table#menuMealsShow").dataTable({
       "language": {
         "url": "https://cdn.datatables.net/plug-ins/1.10.16/i18n/French.json"
@@ -99,48 +99,50 @@ $(document).ready(function(){
 // User Index
 
 $(document).ready(function(){
-  $("table#users_table_id").dataTable({
-    "language": {
-      "url": "https://cdn.datatables.net/plug-ins/1.10.16/i18n/French.json"
-    },
-    "ajax": {
-      "url": "/admin/datatables/getUsers",
-      "type": "GET"
-    },
-    "columns": [
-      { "data": "first_name" },
-      { "data": "last_name" },
-      { "data": "email" },
-      { "data": "role" },
-      {
-        "data": "id",
-        "searchable": false,
-        "orderable": false,
-        "render": function(data, type, row) {
-          return "<a href='/admin/users/"+ data +"' class='btn btn-outline-primary'><i class='fa fa-eye' aria-hidden='true'></i></a><a href='/admin/users/" + data + "/edit' class='btn btn-outline-success'><i class='fa fa-pencil' aria-hidden='true'></i></a><button onClick='archiveUser(" + data + ")' class='btn btn-outline-danger'><i class='fa fa-ban' aria-hidden='true'></i></button>"
-        }
-      }
-    ],
-    "initComplete": function(){
-      $('#users_table_id tfoot th').each( function () {
-        var title = $(this).text();
-        $(this).html( '<input type="text" placeholder="Recherche '+title+'" />' );
-      });
-
-      // DataTable
-      var table = $('#users_table_id').DataTable();
-
-      // Apply the search
-      table.columns().every( function () {
-        var that = this;
-        $( 'input', this.footer() ).on( 'keyup change', function () {
-          if ( that.search() !== this.value ) {
-            that.search( this.value ).draw();
+  if ($('#users_table_id')[0]) {
+    $("table#users_table_id").dataTable({
+      "language": {
+        "url": "https://cdn.datatables.net/plug-ins/1.10.16/i18n/French.json"
+      },
+      "ajax": {
+        "url": "/admin/datatables/getUsers",
+        "type": "GET"
+      },
+      "columns": [
+        { "data": "first_name" },
+        { "data": "last_name" },
+        { "data": "email" },
+        { "data": "role" },
+        {
+          "data": "id",
+          "searchable": false,
+          "orderable": false,
+          "render": function(data, type, row) {
+            return "<a href='/admin/users/"+ data +"' class='btn btn-outline-primary'><i class='fa fa-eye' aria-hidden='true'></i></a><a href='/admin/users/" + data + "/edit' class='btn btn-outline-success'><i class='fa fa-pencil' aria-hidden='true'></i></a><button onClick='archiveUser(" + data + ")' class='btn btn-outline-danger'><i class='fa fa-ban' aria-hidden='true'></i></button>"
           }
+        }
+      ],
+      "initComplete": function(){
+        $('#users_table_id tfoot th').each( function () {
+          var title = $(this).text();
+          $(this).html( '<input type="text" placeholder="Recherche '+title+'" />' );
         });
-      });
-    }
-  });
+
+        // DataTable
+        var table = $('#users_table_id').DataTable();
+
+        // Apply the search
+        table.columns().every( function () {
+          var that = this;
+          $( 'input', this.footer() ).on( 'keyup change', function () {
+            if ( that.search() !== this.value ) {
+              that.search( this.value ).draw();
+            }
+          });
+        });
+      }
+    });
+  }
 });
 
 
@@ -149,151 +151,158 @@ $(document).ready(function(){
 // Provider Index
 
 $(document).ready(function(){
-  $("table#provider_table_id").dataTable({
-    "language": {
-      "url": "https://cdn.datatables.net/plug-ins/1.10.16/i18n/French.json"
-    },
-    "ajax": {
-      "url": "/admin/datatables/getProviders",
-      "type": "GET"
-    },
-    "columns": [
-      { "data": "first_name" },
-      { "data": "last_name" },
-      { "data": "email" },
-      {
-        "data": "id",
-        "searchable": false,
-        "orderable": false,
-        "render": function(data, type, row) {
-          return "<a href='/admin/users/"+ data +"' class='btn btn-outline-primary'><i class='fa fa-eye' aria-hidden='true'></i></a><a href='/admin/users/" + data + "/edit' class='btn btn-outline-success'><i class='fa fa-pencil' aria-hidden='true'></i></a><button onClick='archiveUser(" + data + ")' class='btn btn-outline-danger'><i class='fa fa-ban' aria-hidden='true'></i></button>"
-        }
-      }
-    ],
-    "initComplete": function(){
-      $('#provider_table_id tfoot th').each( function () {
-        var title = $(this).text();
-        $(this).html( '<input type="text" placeholder="Recherche '+title+'" />' );
-      });
-
-      // DataTable
-      var table = $('#provider_table_id').DataTable();
-
-      // Apply the search
-      table.columns().every( function () {
-        var that = this;
-        $( 'input', this.footer() ).on( 'keyup change', function () {
-          if ( that.search() !== this.value ) {
-            that.search( this.value ).draw();
-          }
-        });
-      });
-    }
-  });
-});
-
-$(document).ready(function(){
-  $("table#ingredient_index_tab").dataTable({
-    "language": {
-      "url": "https://cdn.datatables.net/plug-ins/1.10.16/i18n/French.json"
-    },
-    "ajax": {
-      "url": "/admin/datatables/getIngredients",
-      "type": "GET"
-    },
-    "columns": [
-      { "data": "name" },
-      { "data": "price" },
-      { "data": "quantity" },
-      { "data": "buy_counter" },
-      {
-        "data": "id",
-        "searchable": false,
-        "orderable": false,
-        "render": function(data, type, row) {
-          return "<a href='/admin/buyhistory/new/ingredient/"+ data +"' class='btn btn-outline-success'><i class='fa fa-shopping-cart' aria-hidden='true'></i></a>"
-        }
-      }
-    ],
-    "initComplete": function(){
-      $('#ingredient_index_tab tfoot th').each( function () {
-        var title = $(this).text();
-        $(this).html( '<input type="text" placeholder="Recherche '+title+'" />' );
-      });
-
-      // DataTable
-      var table = $('#ingredient_index_tab').DataTable();
-
-      // Apply the search
-      table.columns().every( function () {
-        var that = this;
-        $( 'input', this.footer() ).on( 'keyup change', function () {
-          if ( that.search() !== this.value ) {
-            that.search( this.value ).draw();
-          }
-        });
-      });
-    }
-  });
-});
-
-
-$(document).ready(function(){
-  $("table#history_index_tab").dataTable({
-    "language": {
-      "url": "https://cdn.datatables.net/plug-ins/1.10.16/i18n/French.json"
-    },
-    "ajax": {
-      "url": "/admin/datatables/getHistory",
-      "type": "GET"
-    },
-    "columns": [
-      { "data": "ingredient.name" },
-      { "data": "price" },
-      { "data": "quantity" },
-      { "data": "created_at" },
-      {
-        "data": "ingredient",
-        "render": function(data, type, row) {
-          return data.user.first_name + " " + data.user.last_name;
-        }
+  if ($('#provider_table_id')[0]){
+    $("table#provider_table_id").dataTable({
+      "language": {
+        "url": "https://cdn.datatables.net/plug-ins/1.10.16/i18n/French.json"
       },
-      {
-        "data": "id",
-        "searchable": false,
-        "orderable": false,
-        "render": function(data, type, row) {
-          return "<a href='/admin/buyhistory/"+ data +"' class='btn btn-outline-primary'><i class='fa fa-eye' aria-hidden='true'></i></a>"
-        }
-      }
-    ],
-    "initComplete": function(){
-      $('#history_index_tab tfoot th').each( function () {
-        var title = $(this).text();
-        $(this).html( '<input type="text" placeholder="Recherche '+title+'" />' );
-      });
-
-      // DataTable
-      var table = $('#history_index_tab').DataTable();
-
-      // Apply the search
-      table.columns().every( function () {
-        var that = this;
-        $( 'input', this.footer() ).on( 'keyup change', function () {
-          if ( that.search() !== this.value ) {
-            that.search( this.value ).draw();
+      "ajax": {
+        "url": "/admin/datatables/getProviders",
+        "type": "GET"
+      },
+      "columns": [
+        { "data": "first_name" },
+        { "data": "last_name" },
+        { "data": "email" },
+        {
+          "data": "id",
+          "searchable": false,
+          "orderable": false,
+          "render": function(data, type, row) {
+            return "<a href='/admin/users/"+ data +"' class='btn btn-outline-primary'><i class='fa fa-eye' aria-hidden='true'></i></a><a href='/admin/users/" + data + "/edit' class='btn btn-outline-success'><i class='fa fa-pencil' aria-hidden='true'></i></a><button onClick='archiveUser(" + data + ")' class='btn btn-outline-danger'><i class='fa fa-ban' aria-hidden='true'></i></button>"
           }
+        }
+      ],
+      "initComplete": function(){
+        $('#provider_table_id tfoot th').each( function () {
+          var title = $(this).text();
+          $(this).html( '<input type="text" placeholder="Recherche '+title+'" />' );
         });
-      });
-    }
-  });
+
+        // DataTable
+        var table = $('#provider_table_id').DataTable();
+
+        // Apply the search
+        table.columns().every( function () {
+          var that = this;
+          $( 'input', this.footer() ).on( 'keyup change', function () {
+            if ( that.search() !== this.value ) {
+              that.search( this.value ).draw();
+            }
+          });
+        });
+      }
+    });
+  }
+});
+
+$(document).ready(function(){
+  if ($('#ingredient_index_tab')[0]) {
+    $("table#ingredient_index_tab").dataTable({
+      "language": {
+        "url": "https://cdn.datatables.net/plug-ins/1.10.16/i18n/French.json"
+      },
+      "ajax": {
+        "url": "/admin/datatables/getIngredients",
+        "type": "GET"
+      },
+      "columns": [
+        { "data": "name" },
+        { "data": "price" },
+        { "data": "quantity" },
+        { "data": "buy_counter" },
+        {
+          "data": "id",
+          "searchable": false,
+          "orderable": false,
+          "render": function(data, type, row) {
+            return "<a href='/admin/buyhistory/new/ingredient/"+ data +"' class='btn btn-outline-success'><i class='fa fa-shopping-cart' aria-hidden='true'></i></a>"
+          }
+        }
+      ],
+      "initComplete": function(){
+        $('#ingredient_index_tab tfoot th').each( function () {
+          var title = $(this).text();
+          $(this).html( '<input type="text" placeholder="Recherche '+title+'" />' );
+        });
+
+        // DataTable
+        var table = $('#ingredient_index_tab').DataTable();
+
+        // Apply the search
+        table.columns().every( function () {
+          var that = this;
+          $( 'input', this.footer() ).on( 'keyup change', function () {
+            if ( that.search() !== this.value ) {
+              that.search( this.value ).draw();
+            }
+          });
+        });
+      }
+    });
+  }
+});
+
+
+$(document).ready(function(){
+  if ($('#history_index_tab')[0]) {
+    $("table#history_index_tab").dataTable({
+      "language": {
+        "url": "https://cdn.datatables.net/plug-ins/1.10.16/i18n/French.json"
+      },
+      "ajax": {
+        "url": "/admin/datatables/getHistory",
+        "type": "GET"
+      },
+      "columns": [
+        { "data": "ingredient.name" },
+        { "data": "price" },
+        { "data": "quantity" },
+        { "data": "created_at" },
+        {
+          "data": "ingredient",
+          "render": function(data, type, row) {
+            return data.user.first_name + " " + data.user.last_name;
+          }
+        },
+        {
+          "data": "id",
+          "searchable": false,
+          "orderable": false,
+          "render": function(data, type, row) {
+            return "<a href='/admin/buyhistory/"+ data +"' class='btn btn-outline-primary'><i class='fa fa-eye' aria-hidden='true'></i></a>"
+          }
+        }
+      ],
+      "initComplete": function(){
+        $('#history_index_tab tfoot th').each( function () {
+          var title = $(this).text();
+          $(this).html( '<input type="text" placeholder="Recherche '+title+'" />' );
+        });
+
+        // DataTable
+        var table = $('#history_index_tab').DataTable();
+
+        // Apply the search
+        table.columns().every( function () {
+          var that = this;
+          $( 'input', this.footer() ).on( 'keyup change', function () {
+            if ( that.search() !== this.value ) {
+              that.search( this.value ).draw();
+            }
+          });
+        });
+      }
+    });
+  }
 });
 
 
 $(document).ready(function(){
   var provider_id = $('#current_provider_id');
-  if (provider_id != undefined){
+  if (provider_id[0]){
     $('#minimizer').click();
+    console.log(provider_id);
     $("table#provider_ingredients_index_tab").dataTable({
       "language": {
         "url": "https://cdn.datatables.net/plug-ins/1.10.16/i18n/French.json"
